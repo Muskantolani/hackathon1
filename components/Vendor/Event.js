94a50495eda8d82da8c527b220ae8f8bef76c7e3
Muskan Tolani <114157436+Muskantolani@users.noreply.github.com>
2023-10-04 13:26:04 +0530

    Add files via upload

---
+++ components/Vendor/Event.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Event.css';
+
+export const Vendor = () => {
+  // Sample data for the cards
+  const cardsData = [
+    { title: 'Take flowers' },
+    { title: 'Contact with xyz vendor' },
+    { title: 'Collect Payment' },
+    { title: 'Check budget' },
+  ];
+
+  return (
+    <div className="middle">
+      <div className="card-container">
+        <h1 className='header'>Upcoming Tasks</h1>
+        {cardsData.map((card, index) => (
+          <div key={index} className="card">
+            <div className="card-inner">
+              <h3>{card.title}</h3>
+              <p>{card.description}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Vendor;
---
+++ components/Vendor/Vendor.js
@@ -0,0 +1,56 @@
+import io from "socket.io-client";
+import { useState } from "react";
+import Chat from "./Chat";
+import Contact from './Contact';
+import Event from './Event';
+import Sidebar from './Sidebar';
+
+const socket = io.connect("http://localhost:3001");
+
+function Vendor() {
+  // const [username, setUsername] = useState("");
+  // const [room, setRoom] = useState("");
+  // const [showChat, setShowChat] = useState(false);
+
+  // const joinRoom = () => {
+  //   if (username !== "" && room !== "") {
+  //     socket.emit("join_room", room);
+  //     setShowChat(true);
+  //   }
+  // };
+
+  return (
+    // <div className="Vendor-container">
+    //   {!showChat ? (
+    //     <div className="joinChatContainer">
+    //       <h3>Join A Chat</h3>
+    //       <input
+    //         type="text"
+    //         placeholder="John..."
+    //         onChange={(event) => {
+    //           setUsername(event.target.value);
+    //         }}
+    //       />
+    //       <input
+    //         type="text"
+    //         placeholder="Room ID..."
+    //         onChange={(event) => {
+    //           setRoom(event.target.value);
+    //         }}
+    //       />
+    //       <button onClick={joinRoom}>Join A Room</button>
+    //     </div>
+    //   ) : (
+    //     <Chat socket={socket} username={username} room={room} />
+    //   )}
+    // </div>
+    <>
+    <Event/> 
+    <Sidebar/> 
+    <Contact />
+    </>
+
+  );
+}
+
+export default Vendor;
